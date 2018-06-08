e45dbc4bedb6c383f658ebeae1cad9146c5829df
Arani Nayak <arani.nayak@nl.abnamro.com>
2018-06-08 15:17:52 +0200

    removed redundant headers

--- src/main/java/com/beyondbanking/ssh/financialoverviewservice/controllers/TransactionsController.java
+++ src/main/java/com/beyondbanking/ssh/financialoverviewservice/controllers/TransactionsController.java
@@ -20,9 +20,9 @@ public class TransactionsController {
         headers.set("Content-Type", "application/json");
         headers.set("Authorization", "DirectLogin token=" + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyIiOiIifQ.Y9djraiiTmBz1O6INxhESMpDlFvsHdxa-4Skd4IOQJw");
         // headers.set("Host", "127.0.0.1:8080");
-        headers.set("Connection", "close");
-        headers.set("User-Agent", "Paw/2.3.3 (Macintosh; OS X/10.11.3) GCDHTTPRequest");
-        headers.set("Content-Length", "0");
+//        headers.set("Connection", "close");
+//        headers.set("User-Agent", "Paw/2.3.3 (Macintosh; OS X/10.11.3) GCDHTTPRequest");
+//        headers.set("Content-Length", "0");
         HttpEntity<?> entity = new HttpEntity<>(headers);
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity exchange = restTemplate.exchange(uri, HttpMethod.GET, entity, object);
